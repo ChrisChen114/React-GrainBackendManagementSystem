9349fa4d27f052592964db3d439ec5b9162c1e37
ChrisChen114 <chen.qianfu114@gmail.com>
2022-11-23 13:51:24 -0800

    add left navigation

---
+++ src/components/left-nav/left-nav.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+/*
+* 左侧导航的组件
+* */
+class LeftNav extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default LeftNav;
