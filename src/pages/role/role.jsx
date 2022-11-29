80be5e015723ffd9a5df80b1985f5d17ba7b00ca
ChrisChen114 <chen.qianfu114@gmail.com>
2022-11-29 00:50:53 -0800

    feat(role): add role operations
    add role operations

--- src/pages/role/role.jsx
+++ src/pages/role/role.jsx
@@ -11,6 +11,7 @@ import AddForm from "../role/add-form";
 import AuthForm from "../role/auth-form";
 import memoryUtils from "../../utils/memoryUtils";
 import {formateDate} from "../../utils/dateUtils";
+import storageUtils from "../../utils/storageUtils";
 
 
 /*
@@ -140,11 +141,23 @@ class Role extends Component {
         //    请求更新
         const result = await reqUpdateRole(role);
         if (result.status === 0) {
-            message.success('设置角色权限成功');
-            // this.getRoles();// 这种是最简单的方式
-            this.setState({//这种就是理解上费劲一些
-                roles: [...this.state.roles]
-            })
+
+            // 如果当前更新的是自己角色的权限，强制退出
+            if (role._id === memoryUtils.user.role_id){
+                //
+                memoryUtils.user = {}
+                storageUtils.removeUser()
+                this.props.history.replace('/login')
+                message.success('当前用户角色权限修改了，请重新登录');
+            }else{
+                message.success('设置角色权限成功');
+                // this.getRoles();// 这种是最简单的方式
+                this.setState({//这种就是理解上费劲一些
+                    roles: [...this.state.roles]
+                })
+            }
+
+
         } else {
             message.error('设置角色权限失败');
         }
