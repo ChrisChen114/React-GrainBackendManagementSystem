ee6135f37c8504a4ab3ce48051e532c397547c33
ChrisChen114 <chen.qianfu114@gmail.com>
2022-11-29 16:02:36 -0800

    refactor(index page): use Provider from redux
    use Provider from redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+
 import App from './App';
 import storageUtils from "./utils/storageUtils";
 import memoryUtils from "./utils/memoryUtils";
+import store from './redux/store'
 
 // 读取local中保存的user，保存到内存中
 // 好处：维持登录与自动登录
 const user = storageUtils.getUser();
 memoryUtils.user = user;
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    (
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    ), document.getElementById('root'));
 
